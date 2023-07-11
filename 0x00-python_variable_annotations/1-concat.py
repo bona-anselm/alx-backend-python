6e0eee151633413c32f8d1a14085750264e93f72
Bonaventure Anselm <bona.anselm@gmail.com>
2023-07-11 05:42:12 +0100

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A  type-annotated function concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Function concat that takes a string str1 and a string str2 as
+        arguments and returns a concatenated string
+    """
+    return str1 + str2
