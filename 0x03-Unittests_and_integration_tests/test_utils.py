1ded6f97e77ab3220006b2ff24f68b46a0919795
Bonaventure Anselm <bona.anselm@gmail.com>
2023-07-31 17:15:08 +0100

    Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@ from parameterized import parameterized
 from typing import Dict, Tuple, Union
 import unittest
 import utils
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -31,5 +32,25 @@ class TestAccessNestedMap(unittest.TestCase):
                                          path: Tuple[str],
                                          exception: Exception
                                          ) -> None:
+        """ Tests that utils.access_nested_map returns expected o/p """
         with self.assertRaises(exception):
             utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Tests that utils.get_json returns expected result """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self,
+                      test_url: str,
+                      test_payload: Dict
+                      ) -> Dict:
+        """ Tests that utils.get_json returns expected output """
+        mocked_attrs = {"json.return_value": test_payload}
+        with patch("requests.get", return_value=Mock(**mocked_attrs)) as res:
+            self.assertEqual(utils.get_json(test_url), test_payload)
+            res.assert_called_once_with(test_url)
+
